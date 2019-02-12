ac3ee8788c696c1cc9d8eac58adaaea5a2aeec50
magnop12 <aaakoba12@gmail.com>
2019-02-12 09:26:19 +0900

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-app>
+    <v-toolbar>
+      
+    </v-toolbar>
+
+    <v-content>
+      <HelloWorld/>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data () {
+    return {
+      sideNav: false
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+  @import './stylus/main'
+</style>
---
+++ src/main.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import App from './App'
+import router from './router'
+import 'vuetify/dist/vuetify.min.css'
+import colors from 'vuetify/lib/util/colors'
+
+Vue.use(Vuetify, {
+  theme: {
+    primary: colors.red.darken2,
+    accent: colors.red.accent2,
+    secondary: colors.grey.lighten1,
+    info: colors.blue.lighten1,
+    warning: colors.amber.darken2,
+    error: colors.red.accent4,
+    success: colors.green.lighten2
+  }
+})
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+})
