5f4f5ea3f6dd8393db6e0bf4b239eee3b9a5eebd
magnop12 <aaakoba12@gmail.com>
2019-02-14 02:08:44 +0900

    add toolbar link

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,13 @@
 <template>
   <v-app>
-    <!-- menubar -->
-    <v-toolbar dark class="primary">
-      <v-toolbar-side-icon
-        @click.stop="sideNav = !sideNav"
-        class="hidden-sm-and-up"></v-toolbar-side-icon>
-      <v-toolbar-title>DevMeetUp</v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-toolbar-items class="hidden-xs-only">
-        <v-btn flat v-for="item in MenuItems" :key="item.title">
-          <v-icon left>{{ item.icon }}</v-icon>
-          {{ item.title }}
-        </v-btn>
-      </v-toolbar-items>
-    </v-toolbar>
     <!-- navigation -->
-    <v-navigation-drawer fixed v-model="sideNav">
+    <v-navigation-drawer temporary fixed v-model="sideNav">
       <v-list dense>
-        <v-list-tile v-for="item in MenuItems" :key="item.title">
+        <v-list-tile
+          v-for="item in MenuItems"
+          :key="item.title"
+          router
+          :to="item.link">
           <v-list-tile-action>
             <v-icon>{{ item.icon }}</v-icon>
           </v-list-tile-action>
@@ -25,6 +15,27 @@
         </v-list-tile>
       </v-list>
     </v-navigation-drawer>
+    <!-- menubar -->
+    <v-toolbar dark class="primary">
+      <v-toolbar-side-icon
+        @click.stop="sideNav = !sideNav"
+        class="hidden-sm-and-up"></v-toolbar-side-icon>
+      <v-toolbar-title>
+        <router-link to="/" tag="span" style="cursor: pointer">DevMeetUp</router-link>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-xs-only">
+        <v-btn
+          flat
+          v-for="item in MenuItems"
+          :key="item.title"
+          route
+          :to="item.link">
+          <v-icon left>{{ item.icon }}</v-icon>
+          {{ item.title }}
+        </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
     <!-- content -->
     <main>
       <router-view></router-view>
@@ -44,11 +55,11 @@ export default {
     return {
       sideNav: false,
       MenuItems: [
-        { icon: 'supervisor_account', title: 'View Meetups' },
-        { icon: 'room', title: 'Organize Meetup' },
-        { icon: 'person', title: 'Profile' },
-        { icon: 'face', title: 'Sign up' },
-        { icon: 'lock_open', title: 'Sign in' }
+        { icon: 'supervisor_account', title: 'View Meetups', link: '/meetups' },
+        { icon: 'room', title: 'Organize Meetup', link: '/meetup/new' },
+        { icon: 'person', title: 'Profile', link: '/profile' },
+        { icon: 'face', title: 'Sign up', link: '/signup' },
+        { icon: 'lock_open', title: 'Sign in', link: '/signin' }
       ]
     }
   }
