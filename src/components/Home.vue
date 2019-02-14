c3e7c5201e848f092cdf9054d7452cbcd95a5cc1
magnop12 <39919215+magnop12@users.noreply.github.com>
2019-02-15 00:04:06 +0900

    Add carousel

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,55 @@
 <template>
-    <div>
-        <p>The Home Page</p>
-    </div>
+  <v-container>
+    <v-layout row wrap>
+      <v-flex xs12 sm6 class="text-xs-center text-sm-right">
+        <v-btn large router to="/meetups" class="info">Explorer Meetups</v-btn>
+      </v-flex>
+      <v-flex xs12 sm6 class="text-xs-center text-sm-left">
+        <v-btn large router to="/meetup/new" class="info">Organize Meetups</v-btn>
+      </v-flex>
+    </v-layout>
+    <v-layout row wrap class="mt-2">
+      <v-flex xs12>
+        <v-carousel>
+          <v-carousel-item
+            v-for="meetup in meetups"
+            :key="meetup.id"
+            :src="meetup.imageUrl">
+            <div class="text-xs-center title">
+              {{ meetup.title }}
+            </div>
+          </v-carousel-item>
+        </v-carousel>
+      </v-flex>
+    </v-layout>
+    <v-layout row wrap class="mt-2">
+      <v-flex xs12 class="text-xs-center">
+        <p>Join our awesome meetups!</p>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      meetups: [
+        { id: 'asfafdf', imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/b/bf/Tokyo_Montage_2015.jpg', title: 'Meetup in Tokyo' },
+        { id: 'sda21rf', imageUrl: 'https://cdn.japantimes.2xx.jp/wp-content/uploads/2018/04/n-kyoto-a-20180406-870x580.jpg', title: 'Meetup in Kyoto' }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+.title{
+  position: absolute;
+  bottom: 50px;
+  background-color: rgba(0, 0, 0, 0.5);
+  color: white;
+  font-size: 2em;
+  padding: 20px;
+}
+</style>
